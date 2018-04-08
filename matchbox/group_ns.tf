6adb8568fca3d5af661738dbea26824e3cf313ea
randomcoww <etherblimp@gmail.com>
2018-04-07 18:55:36 -0700

    add ns3

--- matchbox/group_ns.tf
+++ matchbox/group_ns.tf
@@ -4,31 +4,15 @@ resource "matchbox_profile" "ns" {
   container_linux_config = "${file("./ignition/ns.yaml.tmpl")}"
 }
 
-# resource "matchbox_profile" "ns1" {
-#   name   = "ns1"
+# resource "matchbox_profile" "ns" {
+#   name   = "ns"
 #   container_linux_config = "${file("./ignition/ns.yaml.tmpl")}"
 #   kernel = "/assets/coreos/${var.container_linux_version}/coreos_production_pxe.vmlinuz"
 #   initrd = [
 #     "/assets/coreos/${var.container_linux_version}/coreos_production_pxe_image.cpio.gz"
 #   ]
 #   args = [
-#     "ip=192.168.126.219:::255.255.254.0::eth1:none:",
-#     "coreos.config.url=${var.matchbox_http_endpoint}/ignition?mac=$${mac:hexhyp}",
-#     "coreos.first_boot=yes",
-#     "console=hvc0",
-#     "coreos.autologin"
-#   ]
-# }
-#
-# resource "matchbox_profile" "ns2" {
-#   name   = "ns2"
-#   container_linux_config = "${file("./ignition/ns.yaml.tmpl")}"
-#   kernel = "/assets/coreos/${var.container_linux_version}/coreos_production_pxe.vmlinuz"
-#   initrd = [
-#     "/assets/coreos/${var.container_linux_version}/coreos_production_pxe_image.cpio.gz"
-#   ]
-#   args = [
-#     "ip=192.168.126.220:::255.255.254.0::eth1:none:",
+#     "ip=192.168.126.255:::255.255.254.0::eth1:none:",
 #     "coreos.config.url=${var.matchbox_http_endpoint}/ignition?mac=$${mac:hexhyp}",
 #     "coreos.first_boot=yes",
 #     "console=hvc0",
@@ -81,5 +65,28 @@ resource "matchbox_group" "ns2" {
     hyperkube_image = "${var.hyperkube_image}"
     ssh_authorized_key = "cert-authority ${tls_private_key.ssh.public_key_openssh}"
     manifest_url = "https://raw.githubusercontent.com/randomcoww/environment-config/master/manifests/ns2"
+  }
+}
+
+resource "matchbox_group" "ns3" {
+  name    = "ns3"
+  profile = "${matchbox_profile.ns.name}"
+
+  selector {
+    host = "ns3"
+    # mac = "52-54-00-04-fc-ab"
+  }
+
+  metadata {
+    name        = "ns3"
+    lan_ip      = "192.168.62.221"
+    store_ip    = "192.168.126.221"
+    netmask     = "23"
+    gateway_ip  = "${var.gateway_ip}"
+    dns_ip      = "127.0.0.1"
+    default_user   = "${var.default_user}"
+    hyperkube_image = "${var.hyperkube_image}"
+    ssh_authorized_key = "cert-authority ${tls_private_key.ssh.public_key_openssh}"
+    manifest_url = "https://raw.githubusercontent.com/randomcoww/environment-config/master/manifests/ns3"
   }
 }
