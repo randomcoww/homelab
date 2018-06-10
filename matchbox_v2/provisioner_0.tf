b6af5bfcc0d25c241f2e1021a8cc9f6e9533e444
randomcoww <etherblimp@gmail.com>
2018-06-10 16:13:47 -0700

    migrate provisioner to host

--- matchbox_v2/provisioner_0.tf
+++ matchbox_v2/provisioner_0.tf
@@ -3,7 +3,7 @@
 ##
 resource "matchbox_profile" "provisioner_0" {
   name   = "provisioner_0"
-  container_linux_config = "${file("./ignition/provisioner.ign.tmpl")}"
+  generic_config = "${file("./kickstart/provisioner.ks.tmpl")}"
 }
 
 
@@ -23,20 +23,18 @@ resource "matchbox_group" "provisioner_0" {
     hyperkube_image = "${var.hyperkube_image}"
     ssh_authorized_key = "cert-authority ${chomp(tls_private_key.ssh_ca.public_key_openssh)}"
     default_user  = "${var.default_user}"
-    manifest_url  = "https://raw.githubusercontent.com/randomcoww/environment-config/master/manifests/provisioner-0"
 
     ip_lan        = "192.168.62.218"
+    if_lan        = "ens1f1"
     netmask_lan   = "23"
     ip_store      = "192.168.126.218"
+    if_store      = "ens1f0"
     netmask_store = "23"
-    ip_sync       = "192.168.190.218"
-    netmask_sync  = "23"
+    if_wan        = "eno2"
 
-    tls_ca        = "${replace(tls_self_signed_cert.root.cert_pem, "\n", "\\n")}"
-    tls_ca_key    = "${replace(tls_private_key.root.private_key_pem, "\n", "\\n")}"
-    tls_matchbox  = "${replace(tls_locally_signed_cert.matchbox.cert_pem, "\n", "\\n")}"
-    tls_matchbox_key = "${replace(tls_private_key.matchbox.private_key_pem, "\n", "\\n")}"
-    tls_cfssl     = "${replace(tls_locally_signed_cert.cfssl.cert_pem, "\n", "\\n")}"
-    tls_cfssl_key = "${replace(tls_private_key.cfssl.private_key_pem, "\n", "\\n")}"
+    hyperkube_image = "gcr.io/google_containers/hyperkube:v1.10.3"
+    tls_ca        = "${chomp(tls_self_signed_cert.root.cert_pem)}"
+    tls_matchbox  = "${chomp(tls_locally_signed_cert.matchbox.cert_pem)}"
+    tls_matchbox_key = "${chomp(tls_private_key.matchbox.private_key_pem)}"
   }
 }
