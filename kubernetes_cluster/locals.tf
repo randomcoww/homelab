051ee5ac57fb28052ef03ec134768630d5e3356b
randomcoww <randomcoww@myhost-192-168-62-76.host.internal>
2018-08-12 00:13:39 -0700

    Migrate kubernetes to provisioner matchbox

--- kubernetes_cluster/locals.tf
+++ kubernetes_cluster/locals.tf
@@ -1,7 +1,7 @@
 locals {
   kubernetes_version       = "v1.11.2"
   renderer_endpoint        = "127.0.0.1:8081"
-  renderer_cert_pem        = "${file("../renderer/output/server.crt")}"
-  renderer_private_key_pem = "${file("../renderer/output/server.key")}"
-  renderer_ca_pem          = "${file("../renderer/output/ca.crt")}"
+  renderer_cert_pem        = "${file("../provisioner/output/default/server.crt")}"
+  renderer_private_key_pem = "${file("../provisioner/output/default/server.key")}"
+  renderer_ca_pem          = "${file("../provisioner/output/default/ca.crt")}"
 }
