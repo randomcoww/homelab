5e2ce1f750fef307301d6ab5e82db6896c6de91c
randomcoww <randomcoww@randomcowws-MacBook.local>
2018-08-27 21:18:19 -0700

    Remove lan macvlan from store hosts

--- setup_provisioner/store.tf
+++ setup_provisioner/store.tf
@@ -7,18 +7,15 @@ module "store" {
   ssh_ca_public_key = "${tls_private_key.ssh_ca.public_key_openssh}"
 
   ## host configs
-  store_hosts     = ["store-0"]
-  store_lan_ips   = ["192.168.62.251"]
-  store_store_ips = ["192.168.126.251"]
-  store_lan_if    = "ens1f1"
-  store_store_if  = "ens1f0"
+  store_hosts = ["store-0"]
+  store_ips   = ["192.168.126.251"]
+  store_if    = "ens1f0"
 
   ## images
   hyperkube_image = "gcr.io/google_containers/hyperkube:${local.kubernetes_version}"
 
   # ## ip ranges
-  lan_netmask   = "23"
-  store_netmask = "23"
+  netmask = "23"
 
   ## renderer provisioning access
   renderer_endpoint        = "${local.renderer_endpoint}"
