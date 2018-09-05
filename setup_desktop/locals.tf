851cc3c57c57ac4a3b9b72329680a4355eb6f72c
randomcoww <randomcoww@myhost-192-168-126-83.host.internal>
2018-09-04 22:39:54 -0700

    Add packages to desktop kickstart

--- setup_desktop/locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  renderer_endpoint        = "192.168.126.242:58081"
-  renderer_cert_pem        = "${file("../setup_provisioner/output/matchbox/client.crt")}"
-  renderer_private_key_pem = "${file("../setup_provisioner/output/matchbox/client.key")}"
-  renderer_ca_pem          = "${file("../setup_provisioner/output/matchbox/ca.crt")}"
-}
--- setup_desktop/provider.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "randomcoww-tfstate"
-    key     = "desktop.tfstate"
-    region  = "us-west-2"
-    encrypt = true
-  }
-}
