ea19b67c42eec98277cd1378d81e5a119dd2c40a
randomcoww <etherblimp@gmail.com>
2019-01-20 16:31:01 -0800

    Upgrade kubernetes to 1.13.2

--- setup_environment/locals.tf
+++ setup_environment/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  kubernetes_version       = "v1.12.3"
+  kubernetes_version       = "v1.13.2"
   renderer_endpoint        = "192.168.126.242:58081"
   renderer_cert_pem        = "${file("../setup_provisioner/output/matchbox/client.crt")}"
   renderer_private_key_pem = "${file("../setup_provisioner/output/matchbox/client.key")}"
