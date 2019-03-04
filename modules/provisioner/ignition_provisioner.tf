4d284436a746989dcc8976dfbb3dfdf1c842f281
randomcoww <etherblimp@gmail.com>
2019-03-03 17:10:25 -0800

    Move provisioner manifest to manifest path

--- modules/provisioner/ignition_provisioner.tf
+++ modules/provisioner/ignition_provisioner.tf
@@ -6,6 +6,22 @@ resource "matchbox_profile" "ignition_provisioner" {
   container_linux_config = "${file("${path.module}/templates/ignition/provisioner.ign.tmpl")}"
 }
 
+locals {
+  kea_ha_peers_template = {
+    name = "%s"
+    role = "%s"
+    url  = "http://%s:${var.kea_peer_port}/"
+  }
+
+  syncthing_folder_devices_template = <<EOF
+<device id="%s"></device>
+EOF
+
+  syncthing_devices_template = <<EOF
+<device id="%s" compression="always" skipIntroductionRemovals="true"><address>%s:%s</address><allowedNetwork>%s</allowedNetwork></device>
+EOF
+}
+
 resource "matchbox_group" "ignition_provisioner" {
   count = "${length(var.provisioner_hosts)}"
 
@@ -21,7 +37,25 @@ resource "matchbox_group" "ignition_provisioner" {
     hyperkube_image    = "${var.hyperkube_image}"
     ssh_authorized_key = "cert-authority ${chomp(var.ssh_ca_public_key)}"
     default_user       = "${var.default_user}"
-    manifest_url       = "${var.remote_provision_base_url}/manifest/${matchbox_profile.manifest_provisioner.name}.yaml"
+
+    keepalived_image = "${var.keepalived_image}"
+    unbound_image    = "${var.unbound_image}"
+    nftables_image   = "${var.nftables_image}"
+    kea_image        = "${var.kea_image}"
+    tftpd_image      = "${var.tftpd_image}"
+    matchbox_image   = "${var.matchbox_image}"
+    syncthing_image  = "${var.syncthing_image}"
+
+    matchbox_http_port  = "${var.matchbox_http_port}"
+    matchbox_rpc_port   = "${var.matchbox_rpc_port}"
+    kea_peer_port       = "${var.kea_peer_port}"
+    syncthing_peer_port = "${var.syncthing_peer_port}"
+
+    matchbox_vip      = "${var.matchbox_vip}"
+    store_gateway_vip = "${var.store_gateway_vip}"
+    lan_gateway_vip   = "${var.lan_gateway_vip}"
+    dns_vip           = "${var.dns_vip}"
+    backup_dns_ip     = "${var.backup_dns_ip}"
 
     lan_ip        = "${var.provisioner_lan_ips[count.index]}"
     lan_if        = "${var.provisioner_lan_if}"
@@ -32,9 +66,37 @@ resource "matchbox_group" "ignition_provisioner" {
     wan_if        = "${var.provisioner_wan_if}"
     mtu           = "${var.mtu}"
 
-    backup_dns_ip = "${var.backup_dns_ip}"
+    domain_name = "${var.domain_name}"
+
+    lan_ip_range        = "${var.lan_ip_range}"
+    lan_dhcp_ip_range   = "${var.lan_dhcp_ip_range}"
+    store_ip_range      = "${var.store_ip_range}"
+    store_dhcp_ip_range = "${var.store_dhcp_ip_range}"
+
     kubelet_path  = "${var.kubelet_path}"
     certs_path    = "${var.certs_path}"
+    kea_path      = "${var.kea_path}"
+    matchbox_path = "${var.matchbox_path}"
+
+    kea_ha_peers = "${join(",", formatlist(
+      "${jsonencode(local.kea_ha_peers_template)}",
+      "${var.provisioner_hosts}",
+      "${var.kea_ha_roles}",
+      "${var.provisioner_store_ips}"
+    ))}"
+
+    syncthing_folder_devices = "${join("", formatlist(
+      "${chomp(local.syncthing_folder_devices_template)}",
+      "${data.syncthing_device.syncthing.*.device_id}"
+    ))}"
+
+    syncthing_devices = "${join("", formatlist(
+      "${chomp(local.syncthing_devices_template)}",
+      "${data.syncthing_device.syncthing.*.device_id}",
+      "${var.provisioner_store_ips}",
+      "${var.syncthing_peer_port}",
+      "${var.store_ip_range}"
+    ))}"
 
     tls_ca            = "${replace(tls_self_signed_cert.root.cert_pem, "\n", "\\n")}"
     tls_matchbox      = "${replace(element(tls_locally_signed_cert.matchbox.*.cert_pem, count.index), "\n", "\\n")}"
