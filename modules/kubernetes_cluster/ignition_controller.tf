fedb0ed8abf305dfb16d677d5d28fc371c36319a
randomcoww <etherblimp@gmail.com>
2019-03-03 18:59:02 -0800

    Move controller manifest to manifest path

--- modules/kubernetes_cluster/ignition_controller.tf
+++ modules/kubernetes_cluster/ignition_controller.tf
@@ -33,14 +33,39 @@ resource "matchbox_group" "ignition_controller" {
     hyperkube_image    = "${var.hyperkube_image}"
     ssh_authorized_key = "cert-authority ${chomp(var.ssh_ca_public_key)}"
     default_user       = "${var.default_user}"
-    manifest_url       = "http://${var.matchbox_vip}:${var.matchbox_http_port}/generic?manifest=${matchbox_profile.manifest_controller.name}"
     apiserver_url      = "https://127.0.0.1:${var.apiserver_secure_port}"
-    cluster_name       = "${var.cluster_name}"
 
     host_ip      = "${var.controller_ips[count.index]}"
     host_if      = "${var.controller_if}"
     host_netmask = "${var.controller_netmask}"
+
+    keepalived_image              = "${var.keepalived_image}"
+    kube_apiserver_image          = "${var.kube_apiserver_image}"
+    kube_controller_manager_image = "${var.kube_controller_manager_image}"
+    kube_scheduler_image          = "${var.kube_scheduler_image}"
+    etcd_wrapper_image            = "${var.etcd_wrapper_image}"
+    etcd_image                    = "${var.etcd_image}"
+
+    etcd_initial_cluster  = "${join(",", formatlist("%s=https://%s:${var.etcd_peer_port}", "${var.controller_hosts}", "${var.controller_ips}"))}"
+    etcd_cluster_token    = "${var.etcd_cluster_token}"
+    etcd_servers          = "${join(",", formatlist("https://%s:${var.etcd_client_port}", "${var.controller_ips}"))}"
+    etcd_client_port      = "${var.etcd_client_port}"
+    etcd_peer_port        = "${var.etcd_peer_port}"
+    apiserver_secure_port = "${var.apiserver_secure_port}"
+
+    aws_region            = "${var.aws_region}"
+    aws_access_key_id     = "${var.aws_access_key_id}"
+    aws_secret_access_key = "${var.aws_secret_access_key}"
+    s3_backup_path        = "${var.s3_backup_path}"
+
+    cluster_ip_range = "${var.cluster_ip_range}"
+    cluster_cidr     = "${var.cluster_cidr}"
+    cluster_name     = "${var.cluster_name}"
+
+    controller_vip = "${var.controller_vip}"
+
     kubelet_path = "${var.kubelet_path}"
+    etcd_path    = "${var.etcd_path}"
 
     tls_ca                     = "${replace(tls_self_signed_cert.root.cert_pem, "\n", "\\n")}"
     tls_ca_key                 = "${replace(tls_private_key.root.private_key_pem, "\n", "\\n")}"
