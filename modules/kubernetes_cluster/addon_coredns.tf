486b1bd1b1ddc58dff9ed2330671cc2bd12321c7
randomcoww <etherblimp@gmail.com>
2019-03-25 21:04:15 -0700

    Remove unused variables

--- modules/kubernetes_cluster/addon_coredns.tf
+++ modules/kubernetes_cluster/addon_coredns.tf
@@ -15,16 +15,14 @@ resource "matchbox_group" "addon_coredns" {
   }
 
   metadata {
-    coredns_image           = "${var.coredns_image}"
-    external_dns_image      = "${var.external_dns_image}"
-    etcd_image      = "${var.etcd_image}"
-    etcd_endpoints          = "${join(",", formatlist("https://%s:${var.etcd_client_port}", "${var.controller_ips}"))}"
-    corefile_etcd_endpoints = "${join(" ", formatlist("https://%s:${var.etcd_client_port}", "${var.controller_ips}"))}"
-    recursive_dns_vip       = "${var.recursive_dns_vip}"
-    internal_dns_vip        = "${var.internal_dns_vip}"
-    cluster_dns_ip          = "${var.cluster_dns_ip}"
-    cluster_domain          = "${var.cluster_domain}"
-    internal_domain         = "${var.internal_domain}"
-    namespace               = "kube-system"
+    coredns_image      = "${var.coredns_image}"
+    external_dns_image = "${var.external_dns_image}"
+    etcd_image         = "${var.etcd_image}"
+    recursive_dns_vip  = "${var.recursive_dns_vip}"
+    internal_dns_vip   = "${var.internal_dns_vip}"
+    cluster_dns_ip     = "${var.cluster_dns_ip}"
+    cluster_domain     = "${var.cluster_domain}"
+    internal_domain    = "${var.internal_domain}"
+    namespace          = "kube-system"
   }
 }
