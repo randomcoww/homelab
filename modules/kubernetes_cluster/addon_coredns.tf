735d1f7ca6c76fcc13ac5dcce8c4f67abe90577e
randomcoww <etherblimp@gmail.com>
2019-03-25 20:59:34 -0700

    Decouple coredns from cluster etcd

--- modules/kubernetes_cluster/addon_coredns.tf
+++ modules/kubernetes_cluster/addon_coredns.tf
@@ -17,9 +17,9 @@ resource "matchbox_group" "addon_coredns" {
   metadata {
     coredns_image           = "${var.coredns_image}"
     external_dns_image      = "${var.external_dns_image}"
+    etcd_image      = "${var.etcd_image}"
     etcd_endpoints          = "${join(",", formatlist("https://%s:${var.etcd_client_port}", "${var.controller_ips}"))}"
     corefile_etcd_endpoints = "${join(" ", formatlist("https://%s:${var.etcd_client_port}", "${var.controller_ips}"))}"
-    kubelet_path            = "${var.kubelet_path}"
     recursive_dns_vip       = "${var.recursive_dns_vip}"
     internal_dns_vip        = "${var.internal_dns_vip}"
     cluster_dns_ip          = "${var.cluster_dns_ip}"
