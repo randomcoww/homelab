67ec884b3ab60b0266130d736853aa4853571827
randomcoww <etherblimp@gmail.com>
2019-06-04 22:11:24 -0700

    Update certs

--- modules/provisioner/tls_matchbox.tf
+++ modules/provisioner/tls_matchbox.tf
@@ -29,9 +29,9 @@ resource "tls_locally_signed_cert" "matchbox" {
   count = "${length(var.provisioner_hosts)}"
 
   cert_request_pem   = "${element(tls_cert_request.matchbox.*.cert_request_pem, count.index)}"
-  ca_key_algorithm   = "${tls_private_key.root.algorithm}"
-  ca_private_key_pem = "${tls_private_key.root.private_key_pem}"
-  ca_cert_pem        = "${tls_self_signed_cert.root.cert_pem}"
+  ca_key_algorithm   = "${tls_private_key.matchbox_ca.algorithm}"
+  ca_private_key_pem = "${tls_private_key.matchbox_ca.private_key_pem}"
+  ca_cert_pem        = "${tls_self_signed_cert.matchbox_ca.cert_pem}"
 
   validity_period_hours = 8760
 
