d2dd9253cb040026cd1c5706c94eeef33a3887d1
randomcoww <etherblimp@gmail.com>
2019-11-05 22:45:07 -0800

    Remove old resources

--- modules/hardware/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "matchbox" {
-  endpoint    = "${var.renderer_endpoint}"
-  client_cert = "${var.renderer_cert_pem}"
-  client_key  = "${var.renderer_private_key_pem}"
-  ca          = "${var.renderer_ca_pem}"
-}
