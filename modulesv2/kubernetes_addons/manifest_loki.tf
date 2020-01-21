7aa6fd5d72a4748020d0fb414f32fba069a89816
randomcoww <etherblimp@gmail.com>
2020-01-20 21:02:45 -0800

    Add loki manifest

---
+++ modulesv2/kubernetes_addons/manifest_loki.tf
@@ -0,0 +1,19 @@
+##
+## loki addon manifest
+##
+resource "matchbox_group" "manifest-loki" {
+  profile = matchbox_profile.generic-profile.name
+  name    = "loki"
+  selector = {
+    manifest = "loki"
+  }
+
+  metadata = {
+    config = templatefile("${path.module}/../../templates/manifest/loki.yaml.tmpl", {
+      namespace        = "default"
+      services         = var.services
+      networks         = var.networks
+      container_images = var.container_images
+    })
+  }
+}
