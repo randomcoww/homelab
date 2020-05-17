03595841838d68c9da192af0ae61ef1d1342e390
randomcoww <etherblimp@gmail.com>
2020-05-16 17:12:14 -0700

    Update UID and internal CA path

--- modulesv2/desktop_common/outputs.tf
+++ modulesv2/desktop_common/outputs.tf
@@ -4,10 +4,10 @@ output "desktop_params" {
     k => {
       hostname           = k
       user               = var.user
+      uid                = 10000
       password           = var.password
       timezone           = var.timezone
       ssh_authorized_key = ""
-      uid                = 10000
       hosts              = var.desktop_hosts
       host_disks         = var.desktop_hosts[k].disk
       networks           = var.networks
@@ -19,8 +19,8 @@ output "desktop_params" {
         if lookup(var.networks[k], "id", null) != null
       ]
 
-      tls_internal_ca   = chomp(var.internal_ca_cert_pem)
-      internal_tls_path = "/usr/share/pki/ca-trust-source/anchors"
+      tls_internal_ca   = replace(var.internal_ca_cert_pem, "\n", "\\n")
+      internal_tls_path = "/etc/pki/ca-trust/source/anchors"
     }
   }
 }
