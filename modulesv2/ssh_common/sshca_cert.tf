853ab78ed3c2219098d6a176942e118c43b3ea74
randomcoww <etherblimp@gmail.com>
2020-05-25 15:49:31 -0700

    Remove key_id for sharing SSH CA

--- modulesv2/ssh_common/sshca_cert.tf
+++ modulesv2/ssh_common/sshca_cert.tf
@@ -16,7 +16,7 @@ resource "sshca_host_cert" "ssh-host" {
   ca_key_algorithm   = tls_private_key.ssh-ca.algorithm
   ca_private_key_pem = tls_private_key.ssh-ca.private_key_pem
   public_key_openssh = tls_private_key.ssh-host[each.key].public_key_openssh
-  key_id             = each.key
+  # key_id             = each.key
 
   early_renewal_hours   = 8040
   validity_period_hours = 8760
