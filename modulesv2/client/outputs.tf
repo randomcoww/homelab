6058a4dc185f7e109a5f3a70be3e87a7afd7a590
randomcoww <etherblimp@gmail.com>
2020-10-16 23:51:07 -0700

    Refactor pulse audio server config

--- modulesv2/client/outputs.tf
+++ modulesv2/client/outputs.tf
@@ -10,7 +10,9 @@ output "templates" {
         udev_steam_input = data.http.udev-60-steam-input.body
         udev_steam_vr    = data.http.udev-60-steam-vr.body
         wireguard_config = var.wireguard_config
-        swap_device      = var.swap_device
+        pulseaudio_server_networks = [
+          "lan"
+        ]
       })
     ]
   }
