6232338e2eba5d8093b8e6bffe30d32788eae5f3
randomcoww <etherblimp@gmail.com>
2020-11-01 00:00:42 -0700

    Add intermediate resource

--- resourcesv2/resource_kubernetes.tf
+++ resourcesv2/resource_kubernetes.tf
@@ -1,10 +1,19 @@
+# intermediate manifests to force dependencies to run
+data "null_data_source" "kubernetes-manifests" {
+  inputs = merge(
+    module.secrets.addons,
+    module.tls-secrets.addons
+  )
+}
+
+data "null_data_source" "cluster-endpoint" {
+  inputs = module.kubernetes-common.cluster_endpoint
+}
+
 # https://github.com/hashicorp/terraform-provider-kubernetes-alpha
 module "kubernetes-addons" {
   source = "../modulesv2/kubernetes_addons"
 
-  kubernetes_manifests = merge(
-    module.secrets.addons,
-    module.tls-secrets.addons
-  )
-  cluster_endpoint = module.kubernetes-common.cluster_endpoint
+  kubernetes_manifests = data.null_data_source.kubernetes-manifests.outputs
+  cluster_endpoint = data.null_data_source.cluster-endpoint.outputs
 }
