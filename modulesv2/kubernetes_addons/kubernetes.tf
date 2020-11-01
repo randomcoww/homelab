8de3df38b9ab7e091467df8879151c6e481679c6
randomcoww <etherblimp@gmail.com>
2020-11-01 00:05:00 -0700

    Cleanup kubernetes manifest parsing

--- modulesv2/kubernetes_addons/kubernetes.tf
+++ modulesv2/kubernetes_addons/kubernetes.tf
@@ -1,18 +1,12 @@
-locals {
-  manifests = compact(flatten([
-    for k in var.kubernetes_manifests :
-    split("---", k)
-  ]))
-
-  manifest_keys = {
-    for j in local.manifests :
-    "${yamldecode(j).kind}-${lookup(yamldecode(j).metadata, "namespace", "default")}-${yamldecode(j).metadata.name}" => j
-  }
-}
-
 resource "kubernetes_manifest" "manifest" {
   provider = kubernetes-alpha
-  for_each = local.manifest_keys
+  for_each = {
+    for j in compact(flatten([
+      for k in var.kubernetes_manifests :
+      split("---", k)
+    ])) :
+    "${yamldecode(j).kind}-${lookup(yamldecode(j).metadata, "namespace", "default")}-${yamldecode(j).metadata.name}" => j
+  }
 
   manifest = yamldecode(each.value)
 }
