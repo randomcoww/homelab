2c7ab7554e17f9e9f232c003cb12ed12e6f0e716
randomcoww <etherblimp@gmail.com>
2022-01-30 16:23:28 -0800

    Add matchbox ca output

--- modules/pxeboot/outputs.tf
+++ modules/pxeboot/outputs.tf
@@ -1,3 +1,13 @@
 output "manifests" {
   value = local.manifests
 }
+
+output "ca" {
+  value = {
+    matchbox = {
+      algorithm       = tls_private_key.matchbox-ca.algorithm
+      private_key_pem = tls_private_key.matchbox-ca.private_key_pem
+      cert_pem        = tls_self_signed_cert.matchbox-ca.cert_pem
+    }
+  }
+}
