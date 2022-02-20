504106797f925febd5ff59104bd54d2a5406be1f
randomcoww <etherblimp@gmail.com>
2022-02-20 02:13:23 -0800

    Update etcd-wrapper

--- helm_client/helm.tf
+++ helm_client/helm.tf
@@ -200,7 +200,7 @@ resource "helm_release" "mpd" {
   namespace  = "default"
   repository = "https://randomcoww.github.io/terraform-infra/"
   chart      = "mpd"
-  version    = "0.1.2"
+  version    = "0.1.3"
   wait       = false
   values = [
     yamlencode({
