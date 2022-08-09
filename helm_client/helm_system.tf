3ca707f6120dd43bdc462a1fab8c67f3794ac1b4
randomcoww <etherblimp@gmail.com>
2022-08-09 19:21:03 +0000

    revery affinity on kube-dns

--- helm_client/helm_system.tf
+++ helm_client/helm_system.tf
@@ -50,6 +50,22 @@ resource "helm_release" "kube_dns" {
       service = {
         clusterIP = local.vips.cluster_dns
       }
+      affinity = {
+        nodeAffinity = {
+          requiredDuringSchedulingIgnoredDuringExecution = {
+            nodeSelectorTerms = [
+              {
+                matchExpressions = [
+                  {
+                    key      = "role-gateway"
+                    operator = "Exists"
+                  },
+                ]
+              },
+            ]
+          }
+        }
+      }
       priorityClassName = "system-cluster-critical"
       servers = [
         {
