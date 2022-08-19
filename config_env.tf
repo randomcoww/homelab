d0926ec42207d94795a69d38f885fb163013b5d5
randomcoww <etherblimp@gmail.com>
2022-08-19 06:09:30 +0000

    update flannel cni image

--- config_env.tf
+++ config_env.tf
@@ -110,7 +110,7 @@ locals {
     mpd                = "ghcr.io/randomcoww/mpd:0.23.8-2"
     ympd               = "ghcr.io/randomcoww/ympd:20220807"
     flannel            = "ghcr.io/randomcoww/flannel:v0.18.1"
-    flannel_cni_plugin = "rancher/mirrored-flannelcni-flannel-cni-plugin:v1.1.0"
+    flannel_cni_plugin = "docker.io/flannelcni/flannel-cni-plugin:v1.1.0"
     kapprover          = "ghcr.io/randomcoww/kapprover:latest"
     external_dns       = "k8s.gcr.io/external-dns/external-dns:v0.12.0"
     kube_proxy         = "ghcr.io/randomcoww/kubernetes:kube-proxy-v1.24.1"
