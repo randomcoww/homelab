74a5987359f74dc488081a2921b7bb459cd7c801
randomcoww <etherblimp@gmail.com>
2022-11-02 20:34:53 +0000

    Update deprecated http data

--- ignition_config/modules/desktop/locals.tf
+++ ignition_config/modules/desktop/locals.tf
@@ -3,8 +3,8 @@ locals {
     for f in fileset(".", "${path.module}/ignition/*.yaml") :
     templatefile(f, {
       ssh_ca_authorized_key = var.ssh_ca_public_key_openssh
-      udev_steam_input      = data.http.udev-60-steam-input.body
-      udev_steam_vr         = data.http.udev-60-steam-vr.body
+      udev_steam_input      = data.http.udev-60-steam-input.response_body
+      udev_steam_vr         = data.http.udev-60-steam-vr.response_body
       wlan_interface        = var.wlan_interface
     })
   ]
