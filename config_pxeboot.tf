f20893b07806574dbc670a6bebb5fe09ce724953
randomcoww <etherblimp@gmail.com>
2022-11-17 19:26:18 -0800

    update client image

--- config_pxeboot.tf
+++ config_pxeboot.tf
@@ -1,7 +1,7 @@
 locals {
   pxeboot_image_builds = {
     coreos     = "fedora-coreos-37.20221116.0"
-    silverblue = "fedora-silverblue-37.20221116.0"
+    silverblue = "fedora-silverblue-37.20221118.0"
   }
 
   image_set = {
