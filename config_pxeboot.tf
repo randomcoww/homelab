5e3d11a25bf42241ef6cebf8a882d004f9224b7e
randomcoww <averyfuzzybunny@gmail.com>
2022-11-30 23:07:10 -0800

    update fedora image

--- config_pxeboot.tf
+++ config_pxeboot.tf
@@ -1,7 +1,7 @@
 locals {
   pxeboot_image_builds = {
     coreos     = "fedora-coreos-37.20221130.0"
-    silverblue = "fedora-silverblue-37.20221130.0"
+    silverblue = "fedora-silverblue-37.20221201.0"
   }
 
   image_set = {
