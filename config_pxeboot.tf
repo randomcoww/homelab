3f217c899a100662ea2995a047dbc742be2e3d4c
randomcoww <averyfuzzybunny@gmail.com>
2023-01-04 12:46:06 -0800

    update silverblue image

--- config_pxeboot.tf
+++ config_pxeboot.tf
@@ -1,7 +1,7 @@
 locals {
   pxeboot_image_builds = {
     coreos     = "fedora-coreos-37.20221228.0"
-    silverblue = "fedora-silverblue-37.20221231.0"
+    silverblue = "fedora-silverblue-37.20230104.0"
   }
 
   image_set = {
