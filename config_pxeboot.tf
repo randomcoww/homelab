32a927ad4e5d7262960db294343456f2085d719d
randomcoww <averyfuzzybunny@gmail.com>
2023-07-28 15:53:36 -0700

    add nvidia params

--- config_pxeboot.tf
+++ config_pxeboot.tf
@@ -1,7 +1,7 @@
 locals {
   pxeboot_image_builds = {
     coreos     = "fedora-coreos-38.20230701.0"
-    silverblue = "fedora-silverblue-38.20230727.0"
+    silverblue = "fedora-silverblue-38.20230728.0"
   }
 
   image_set = {
@@ -40,6 +40,9 @@ locals {
         ignition = "de-0"
         boot_args = [
           "enforcing=0",
+          "rd.driver.blacklist=nouveau",
+          "modprobe.blacklist=nouveau",
+          "nvidia_drm.modeset=1",
           # "vfio-pci.ids=10de:ffffffff:ffffffff:ffffffff:00030000:ffff00ff,10de:ffffffff:ffffffff:ffffffff:00040300:ffffffff",
           # "vfio-pci.ids=1002:ffffffff:ffffffff:ffffffff:00030000:ffff00ff,1002:ffffffff:ffffffff:ffffffff:00040300:ffffffff,10de:ffffffff:ffffffff:ffffffff:00030000:ffff00ff,10de:ffffffff:ffffffff:ffffffff:00040300:ffffffff",
           # "video=efifb:off",
