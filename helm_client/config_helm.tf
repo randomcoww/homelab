131abb6b2df2841c6db326d846791995b2eec241
randomcoww <averyfuzzybunny@gmail.com>
2023-08-05 23:12:38 -0700

    add websocket upgrade support to ingress nginx

--- helm_client/config_helm.tf
+++ helm_client/config_helm.tf
@@ -9,6 +9,12 @@ locals {
     "nginx.ingress.kubernetes.io/auth-url"              = "http://${local.kubernetes_service_endpoints.authelia}/api/verify"
     "nginx.ingress.kubernetes.io/auth-signin"           = "https://${local.kubernetes_ingress_endpoints.auth}?rm=$request_method"
     "nginx.ingress.kubernetes.io/auth-response-headers" = "Remote-User,Remote-Name,Remote-Groups,Remote-Email"
+    "nginx.ingress.kubernetes.io/http-snippet"          = <<EOF
+map $http_upgrade $connection_upgrade {
+  default upgrade;
+  '' close;
+}
+EOF
     "nginx.ingress.kubernetes.io/auth-snippet"          = <<EOF
 proxy_set_header X-Forwarded-Method $request_method;
 EOF
@@ -20,6 +26,11 @@ add_header X-XSS-Protection "0" always;
 add_header Permissions-Policy "interest-cohort=()";
 add_header Pragma "no-cache";
 add_header Cache-Control "no-store";
+EOF
+    "nginx.ingress.kubernetes.io/location-snippets"     = <<EOF
+proxy_http_version 1.1;
+proxy_set_header Upgrade $http_upgrade;
+proxy_set_header Connection $connection_upgrade;
 EOF
   }
 
