a3dd0a66d48952f6b7d4353a010dcb0ffbebff65
randomcoww <averyfuzzybunny@gmail.com>
2023-08-27 12:09:26 -0700

    add de-1

--- config_hosts.tf
+++ config_hosts.tf
@@ -242,21 +242,89 @@ locals {
         },
       ]
     }
+    de-1 = {
+      netnum = 9
+      users = [
+        "client",
+      ]
+      hardware_interfaces = {
+        phy0 = {
+          mac   = "74-56-3c-c3-10-68"
+          mtu   = 9000
+          vlans = ["kubernetes"]
+        }
+        # mobile
+        phy1 = {
+          mac = "32-57-14-7a-aa-10"
+        }
+      }
+      wlan_interfaces = {
+        wlan0 = {
+          mac         = "10-6f-d9-cf-d5-71"
+          enable_dhcp = true
+          enable_mdns = true
+          metric      = 2048
+        }
+      }
+      bridge_interfaces = {
+        br-lan = {
+          interfaces = ["phy0"]
+        }
+      }
+      tap_interfaces = {
+        lan = {
+          source_interface_name = "br-lan"
+          enable_dhcp           = true
+        }
+        kubernetes = {
+          source_interface_name = "phy0-kubernetes"
+          enable_netnum         = true
+        }
+        fallback = {
+          source_interface_name = "phy1"
+          enable_dhcp           = true
+        }
+      }
+      disks = {
+        # pv = {
+        #   device = "/dev/disk/by-id/nvme-Samsung_SSD_970_EVO_Plus_1TB_S6S1NS0W106465F"
+        #   partitions = [
+        #     {
+        #       mount_path = local.mounts.home_path
+        #       format     = "xfs"
+        #       wipe       = false
+        #       options    = ["-s", "size=4096"]
+        #     },
+        #   ]
+        # }
+      }
+      persistent_path = local.mounts.home_path
+      kubernetes_worker_taints = [
+        {
+          key    = "node-role.kubernetes.io/de"
+          effect = "NoSchedule"
+        },
+        {
+          key    = "node-role.kubernetes.io/de"
+          effect = "NoExecute"
+        },
+      ]
+    }
   }
 
   base_members = {
-    base              = ["gw-0", "gw-1", "q-0", "de-0"]
-    systemd-networkd  = ["gw-0", "gw-1", "q-0", "de-0"]
+    base              = ["gw-0", "gw-1", "q-0", "de-0", "de-1"]
+    systemd-networkd  = ["gw-0", "gw-1", "q-0", "de-0", "de-1"]
     network-manager   = []
-    kubelet-base      = ["gw-0", "gw-1", "q-0", "de-0"]
+    kubelet-base      = ["gw-0", "gw-1", "q-0", "de-0", "de-1"]
     gateway           = ["gw-0", "gw-1", "q-0"]
     vrrp              = ["gw-0", "gw-1"]
-    disks             = ["gw-0", "gw-1", "q-0", "de-0"]
-    ssh-server        = ["gw-0", "gw-1", "q-0", "de-0"]
+    disks             = ["gw-0", "gw-1", "q-0", "de-0", "de-1"]
+    ssh-server        = ["gw-0", "gw-1", "q-0", "de-0", "de-1"]
     etcd              = ["gw-0", "gw-1", "q-0"]
     kubernetes-master = ["gw-0", "gw-1"]
-    kubernetes-worker = ["gw-0", "gw-1", "q-0", "de-0"]
-    desktop           = ["de-0"]
+    kubernetes-worker = ["gw-0", "gw-1", "q-0", "de-0", "de-1"]
+    desktop           = ["de-0", "de-1"]
   }
 
   hosts = {
--- config_pxeboot.tf
+++ config_pxeboot.tf
@@ -48,6 +48,18 @@ locals {
           # "video=efifb:off",
         ]
       })
+      "74-56-3c-c3-10-68" = merge(local.image_set.silverblue, {
+        ignition = "de-1"
+        boot_args = [
+          "enforcing=0",
+          "rd.driver.blacklist=nouveau",
+          "modprobe.blacklist=nouveau",
+          "nvidia_drm.modeset=1",
+          # "vfio-pci.ids=10de:ffffffff:ffffffff:ffffffff:00030000:ffff00ff,10de:ffffffff:ffffffff:ffffffff:00040300:ffffffff",
+          # "vfio-pci.ids=1002:ffffffff:ffffffff:ffffffff:00030000:ffff00ff,1002:ffffffff:ffffffff:ffffffff:00040300:ffffffff,10de:ffffffff:ffffffff:ffffffff:00030000:ffff00ff,10de:ffffffff:ffffffff:ffffffff:00040300:ffffffff",
+          # "video=efifb:off",
+        ]
+      })
     }
   }
 }
