2cda62a861bb9f84ceae89bdac9ca74c550dd59c
randomcoww <averyfuzzybunny@gmail.com>
2024-01-08 15:11:48 -0800

    remove unused variables

--- helm_client/variables.tf
+++ helm_client/variables.tf
@@ -1,44 +1,12 @@
-variable "users" {
-  type    = any
-  default = {}
-}
-
 variable "letsencrypt" {
   type = object({
     email = string
   })
 }
 
-variable "hostapd" {
-  type = map(string)
-}
-
-variable "authelia_users" {
-  type    = any
-  default = {}
-}
-
-variable "wireguard_client" {
-  type = object({
-    private_key = string
-    public_key  = string
-    address     = string
-    endpoint    = string
-  })
-}
-
 variable "tailscale" {
   type = object({
     auth_key = string
-  })
-}
-
-variable "smtp" {
-  type = object({
-    host     = string
-    port     = string
-    username = string
-    password = string
   })
 }
 
