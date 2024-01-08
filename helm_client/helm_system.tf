c1f76ea9301944b15cad571687fde1aacb1a1368
randomcoww <averyfuzzybunny@gmail.com>
2024-01-08 13:41:19 -0800

    reorganize helm release resource files

--- helm_client/helm_system.tf
+++ helm_client/helm_system.tf
@@ -1,32 +1,3 @@
-# basic system #
-
-resource "helm_release" "cluster-services" {
-  name       = "cluster-services"
-  namespace  = "kube-system"
-  repository = "https://randomcoww.github.io/repos/helm/"
-  chart      = "cluster-services"
-  wait       = false
-  version    = "0.2.5"
-  values = [
-    yamlencode({
-      images = {
-        flannelCNIPlugin = local.container_images.flannel_cni_plugin
-        flannel          = local.container_images.flannel
-        kapprover        = local.container_images.kapprover
-        kubeProxy        = local.container_images.kube_proxy
-      }
-      ports = {
-        kubeProxy = local.ports.kube_proxy
-        apiServer = local.ports.apiserver
-      }
-      apiServerIP      = local.services.apiserver.ip
-      cniInterfaceName = local.kubernetes.cni_bridge_interface_name
-      podNetworkPrefix = local.networks.kubernetes_pod.prefix
-      internalDomain   = local.domains.internal
-    }),
-  ]
-}
-
 # local-storage storage class #
 
 resource "helm_release" "local-path-provisioner" {
@@ -470,3 +441,147 @@ resource "helm_release" "cert-issuer" {
     ]
   }
 }
+
+# minio #
+
+resource "helm_release" "minio" {
+  name             = split(".", local.kubernetes_service_endpoints.minio)[0]
+  namespace        = split(".", local.kubernetes_service_endpoints.minio)[1]
+  repository       = "https://charts.min.io/"
+  chart            = "minio"
+  create_namespace = true
+  wait             = true
+  timeout          = 600
+  version          = "5.0.14"
+  values = [
+    yamlencode({
+      clusterDomain = local.domains.kubernetes
+      mode          = "distributed"
+      rootUser      = data.terraform_remote_state.sr.outputs.minio.access_key_id
+      rootPassword  = data.terraform_remote_state.sr.outputs.minio.secret_access_key
+      persistence = {
+        storageClass = "local-path"
+      }
+      drivesPerNode = 2
+      replicas      = 3
+      resources = {
+        requests = {
+          memory = "8Gi"
+        }
+      }
+      service = {
+        type = "LoadBalancer"
+        port = local.service_ports.minio
+        externalIPs = [
+          local.services.minio.ip,
+        ]
+        annotations = {
+          "external-dns.alpha.kubernetes.io/hostname" = local.kubernetes_ingress_endpoints.minio
+        }
+      }
+      # ingress = {
+      #   enabled          = true
+      #   ingressClassName = local.ingress_classes.ingress_nginx
+      #   annotations      = local.nginx_ingress_annotations
+      #   tls = [
+      #     local.tls_wildcard,
+      #   ]
+      #   hosts = [
+      #     local.kubernetes_ingress_endpoints.minio,
+      #   ]
+      # }
+      environment = {
+        MINIO_API_REQUESTS_DEADLINE  = "2m"
+        MINIO_STORAGE_CLASS_STANDARD = "EC:2"
+        MINIO_STORAGE_CLASS_RRS      = "EC:2"
+      }
+      buckets = [
+        for bucket in local.minio_buckets :
+        merge(bucket, {
+          purge      = false
+          versioning = false
+        })
+      ]
+      users          = []
+      policies       = []
+      customCommands = []
+      svcaccts       = []
+      affinity = {
+        nodeAffinity = {
+          requiredDuringSchedulingIgnoredDuringExecution = {
+            nodeSelectorTerms = [
+              {
+                matchExpressions = [
+                  {
+                    key      = "minio"
+                    operator = "Exists"
+                  },
+                ]
+              },
+            ]
+          }
+        }
+        podAntiAffinity = {
+          requiredDuringSchedulingIgnoredDuringExecution = [
+            {
+              labelSelector = {
+                matchExpressions = [
+                  {
+                    key      = "app"
+                    operator = "In"
+                    values = [
+                      "minio",
+                    ]
+                  },
+                ]
+              }
+              topologyKey = "kubernetes.io/hostname"
+            },
+          ]
+        }
+      }
+    }),
+  ]
+}
+
+# mayastor #
+/*
+resource "helm_release" "mayastor" {
+  name             = "mayastor"
+  namespace        = "mayastor"
+  repository       = "https://openebs.github.io/mayastor-extensions/"
+  chart            = "mayastor"
+  create_namespace = true
+  wait             = false
+  version          = "2.5.0"
+  values = [
+    yamlencode({
+      base = {
+        metrics = {
+          enabled = false
+        }
+        jaeger = {
+          enabled = false
+        }
+      }
+      etcd = {
+        clusterDomain = local.domains.kubernetes
+        persistence = {
+          storageClass = "local-path"
+        }
+      }
+      eventing = {
+        enabled = false
+      }
+      loki-stack = {
+        enabled = false
+      }
+      obs = {
+        callhome = {
+          enabled = false
+        }
+      }
+    }),
+  ]
+}
+*/
