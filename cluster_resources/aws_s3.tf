e5e2e76812e4c2f2043346b367ef801e30624a6f
randomcoww <averyfuzzybunny@gmail.com>
2024-01-25 23:08:49 -0800

    add s3 bucket lifecycle and server side encryption configs

--- cluster_resources/aws_s3.tf
+++ cluster_resources/aws_s3.tf
@@ -6,6 +6,29 @@ resource "aws_s3_bucket" "bucket" {
   bucket = each.key
 }
 
+resource "aws_s3_bucket_lifecycle_configuration" "bucket" {
+  for_each = aws_s3_bucket.bucket
+  bucket   = each.key
+  rule {
+    id     = each.key
+    status = "Enabled"
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucket" {
+  for_each = aws_s3_bucket.bucket
+  bucket   = each.key
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
 resource "aws_iam_user" "s3" {
   for_each = local.s3_resources
   name     = each.key
