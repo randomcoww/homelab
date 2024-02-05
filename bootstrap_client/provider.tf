0de0da0436287887563bc3c52c1f1f2bcdcd7db1
randomcoww <averyfuzzybunny@gmail.com>
2024-02-05 00:56:48 -0800

    update cert output format for consistency

--- bootstrap_client/provider.tf
+++ bootstrap_client/provider.tf
@@ -1,6 +1,6 @@
 provider "matchbox" {
   endpoint    = local.matchbox_api_endpoint
-  client_cert = data.terraform_remote_state.bootstrap-server.outputs.matchbox_client.cert_pem
-  client_key  = data.terraform_remote_state.bootstrap-server.outputs.matchbox_client.private_key_pem
+  client_cert = data.terraform_remote_state.bootstrap-server.outputs.matchbox.client_cert.cert_pem
+  client_key  = data.terraform_remote_state.bootstrap-server.outputs.matchbox.client_cert.private_key_pem
   ca          = data.terraform_remote_state.bootstrap-server.outputs.matchbox.ca.cert_pem
 }
--- bootstrap_server/outputs.tf
+++ bootstrap_server/outputs.tf
@@ -7,20 +7,18 @@ output "matchbox_endpoint" {
   value = "http://${local.listen_ip}:${local.service_ports.matchbox}"
 }
 
-output "matchbox_ca" {
+output "matchbox" {
   value = {
-    algorithm       = tls_private_key.matchbox-ca.algorithm
-    private_key_pem = tls_private_key.matchbox-ca.private_key_pem
-    cert_pem        = tls_self_signed_cert.matchbox-ca.cert_pem
-  }
-  sensitive = true
-}
-
-output "matchbox_client" {
-  value = {
-    algorithm       = tls_private_key.matchbox-client.algorithm
-    private_key_pem = tls_private_key.matchbox-client.private_key_pem
-    cert_pem        = tls_locally_signed_cert.matchbox-client.cert_pem
+    ca = {
+      algorithm       = tls_private_key.matchbox-ca.algorithm
+      private_key_pem = tls_private_key.matchbox-ca.private_key_pem
+      cert_pem        = tls_self_signed_cert.matchbox-ca.cert_pem
+    }
+    client_cert = {
+      algorithm       = tls_private_key.matchbox-client.algorithm
+      private_key_pem = tls_private_key.matchbox-client.private_key_pem
+      cert_pem        = tls_locally_signed_cert.matchbox-client.cert_pem
+    }
   }
   sensitive = true
 }
