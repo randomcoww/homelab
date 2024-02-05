6804358814ebb25f4dbb04aa43b75d35463c1808
randomcoww <averyfuzzybunny@gmail.com>
2024-02-05 02:09:04 -0800

    fix to updated output format

--- client_credentials/tls_ssh_client.tf
+++ client_credentials/tls_ssh_client.tf
@@ -1,5 +1,5 @@
 resource "ssh_user_cert" "ssh-client" {
-  ca_private_key_pem    = data.terraform_remote_state.sr.outputs.ssh_ca.private_key_pem
+  ca_private_key_pem    = data.terraform_remote_state.sr.outputs.ssh.ca.private_key_pem
   public_key_openssh    = var.ssh_client.public_key_openssh
   key_id                = var.ssh_client.key_id
   early_renewal_hours   = var.ssh_client.early_renewal_hours
