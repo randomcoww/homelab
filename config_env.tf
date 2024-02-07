408834fb15f7e12ef5ffe6f08a86682d3845166a
randomcoww <averyfuzzybunny@gmail.com>
2024-02-07 04:01:32 -0800

    update mpd cache file paths

--- config_env.tf
+++ config_env.tf
@@ -97,7 +97,7 @@ locals {
     hostapd            = "ghcr.io/randomcoww/hostapd:2.10-2"
     syncthing          = "docker.io/syncthing/syncthing:1.27"
     rclone             = "docker.io/rclone/rclone:1.65"
-    mpd                = "ghcr.io/randomcoww/mpd:0.23.15.2-s6"
+    mpd                = "ghcr.io/randomcoww/mpd:0.23.15.3-s6"
     mympd              = "ghcr.io/jcorporation/mympd/mympd:13.0.6"
     flannel            = "docker.io/flannel/flannel:v0.24.0"
     flannel_cni_plugin = "docker.io/flannel/flannel-cni-plugin:v1.2.0"
