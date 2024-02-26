a7d045df0240f5732e67c4ff3a59135e0ddefe1f
randomcoww <averyfuzzybunny@gmail.com>
2024-02-26 13:36:05 -0800

    update etcd-wrapper

--- config_env.tf
+++ config_env.tf
@@ -89,7 +89,7 @@ locals {
     kube_apiserver          = "ghcr.io/randomcoww/kubernetes:kube-master-v${local.kubernetes.version}"
     kube_controller_manager = "ghcr.io/randomcoww/kubernetes:kube-master-v${local.kubernetes.version}"
     kube_scheduler          = "ghcr.io/randomcoww/kubernetes:kube-master-v${local.kubernetes.version}"
-    etcd_wrapper            = "ghcr.io/randomcoww/etcd-wrapper:20240226.1"
+    etcd_wrapper            = "ghcr.io/randomcoww/etcd-wrapper:20240226.2"
     etcd                    = "gcr.io/etcd-development/etcd:v3.5.12-amd64"
 
     # Helm
