6672a94b7bdcc601da658205a61e50c8d6c5342a
randomcoww <averyfuzzybunny@gmail.com>
2024-04-06 12:11:09 -0700

    add remote to de-0, update alpaca-stream

--- config_hosts.tf
+++ config_hosts.tf
@@ -346,7 +346,7 @@ locals {
     nvidia-container    = ["de-1"]
     desktop-environment = ["de-0", "de-1", "r-0"]
     sunshine            = ["de-1"]
-    remote              = ["r-0"]
+    remote              = ["de-0", "r-0"]
   }
 
   # finalized local vars #
