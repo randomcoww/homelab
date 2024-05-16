161de4b7920fecd0befed9c7ccc7b02a62e72ff5
randomcoww <averyfuzzybunny@gmail.com>
2024-05-16 21:21:26 +0000

    update silverblue image with new sunshine

--- config_env.tf
+++ config_env.tf
@@ -121,7 +121,7 @@ locals {
 
   pxeboot_images = {
     coreos     = "fedora-coreos-39.20240304.0"
-    silverblue = "fedora-silverblue-39.20240513.0"
+    silverblue = "fedora-silverblue-39.20240516.0"
   }
 
   kubernetes = {
