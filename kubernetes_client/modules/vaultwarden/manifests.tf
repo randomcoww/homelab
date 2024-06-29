298bba067360278d73a3dc9167e103e11a8642aa
randomcoww <averyfuzzybunny@gmail.com>
2024-06-28 19:24:00 -0700

    migrate vaultwarden to litestream template

--- kubernetes_client/modules/vaultwarden/manifests.tf
+++ kubernetes_client/modules/vaultwarden/manifests.tf
@@ -1,6 +1,6 @@
 locals {
   db_path = "/data/db.sqlite3"
-  extra_envs = merge(var.extra_configs, {
+  extra_configs = merge(var.extra_configs, {
     DATA_FOLDER  = dirname(local.db_path)
     DATABASE_URL = local.db_path
     ROCKET_PORT  = local.ports.vaultwarden
@@ -18,10 +18,11 @@ module "metadata" {
   release     = var.release
   app_version = split(":", var.images.vaultwarden)[1]
   manifests = {
-    "templates/secret.yaml"      = module.secret.manifest
-    "templates/service.yaml"     = module.service.manifest
-    "templates/ingress.yaml"     = module.ingress.manifest
-    "templates/statefulset.yaml" = module.statefulset.manifest
+    "templates/secret.yaml"            = module.secret.manifest
+    "templates/service.yaml"           = module.service.manifest
+    "templates/ingress.yaml"           = module.ingress.manifest
+    "templates/statefulset.yaml"       = module.statefulset-litestream.statefulset
+    "templates/secret-litestream.yaml" = module.statefulset-litestream.secret
   }
 }
 
@@ -34,7 +35,7 @@ module "secret" {
     ACCESS_KEY_ID     = var.s3_access_key_id
     SECRET_ACCESS_KEY = var.s3_secret_access_key
     }, {
-    for k, v in local.extra_envs :
+    for k, v in local.extra_configs :
     tostring(k) => tostring(v)
   })
 }
@@ -78,62 +79,40 @@ module "ingress" {
   ]
 }
 
-module "statefulset" {
-  source   = "../statefulset"
+module "statefulset-litestream" {
+  source = "../statefulset_litestream"
+  ## litestream settings
+  litestream_image = var.images.litestream
+  litestream_config = {
+    dbs = [
+      {
+        path = local.db_path
+        replicas = [
+          {
+            url               = "s3://${var.s3_db_resource}/${basename(local.db_path)}"
+            access-key-id     = var.s3_access_key_id
+            secret-access-key = var.s3_secret_access_key
+          },
+        ]
+      }
+    ]
+  }
+  sqlite_path = local.db_path
+  ##
   name     = var.name
   app      = var.name
   release  = var.release
   affinity = var.affinity
-  replicas = 1
   annotations = {
     "checksum/secret" = sha256(module.secret.manifest)
   }
   spec = {
-    initContainers = [
-      {
-        name  = "${var.name}-init"
-        image = var.images.litestream
-        args = [
-          "restore",
-          "-if-replica-exists",
-          "-o",
-          local.db_path,
-          "s3://${var.s3_db_resource}",
-        ]
-        env = [
-          {
-            name = "LITESTREAM_ACCESS_KEY_ID"
-            valueFrom = {
-              secretKeyRef = {
-                name = module.secret.name
-                key  = "ACCESS_KEY_ID"
-              }
-            }
-          },
-          {
-            name = "LITESTREAM_SECRET_ACCESS_KEY"
-            valueFrom = {
-              secretKeyRef = {
-                name = module.secret.name
-                key  = "SECRET_ACCESS_KEY"
-              }
-            }
-          },
-        ]
-        volumeMounts = [
-          {
-            name      = "vaultwarden-data"
-            mountPath = dirname(local.db_path)
-          },
-        ]
-      },
-    ]
     containers = [
       {
         name  = var.name
         image = var.images.vaultwarden
         env = [
-          for k, v in local.extra_envs :
+          for k, v in local.extra_configs :
           {
             name = tostring(k)
             valueFrom = {
@@ -144,60 +123,11 @@ module "statefulset" {
             }
           }
         ]
-        volumeMounts = [
-          {
-            name      = "vaultwarden-data"
-            mountPath = dirname(local.db_path)
-          },
-        ]
         ports = [
           {
             containerPort = local.ports.vaultwarden
           },
         ]
-      },
-      {
-        name  = "${var.name}-litestream"
-        image = var.images.litestream
-        args = [
-          "replicate",
-          local.db_path,
-          "s3://${var.s3_db_resource}",
-        ]
-        env = [
-          {
-            name = "LITESTREAM_ACCESS_KEY_ID"
-            valueFrom = {
-              secretKeyRef = {
-                name = module.secret.name
-                key  = "ACCESS_KEY_ID"
-              }
-            }
-          },
-          {
-            name = "LITESTREAM_SECRET_ACCESS_KEY"
-            valueFrom = {
-              secretKeyRef = {
-                name = module.secret.name
-                key  = "SECRET_ACCESS_KEY"
-              }
-            }
-          },
-        ]
-        volumeMounts = [
-          {
-            name      = "vaultwarden-data"
-            mountPath = dirname(local.db_path)
-          },
-        ]
-      },
-    ]
-    volumes = [
-      {
-        name = "vaultwarden-data"
-        emptyDir = {
-          medium = "Memory"
-        }
       },
     ]
   }
