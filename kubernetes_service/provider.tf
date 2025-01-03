f6cad62e768010998d43b813d498b2277289f878
randomcoww <averyfuzzybunny@gmail.com>
2025-01-02 19:00:52 -0800

    use service apiserver endpoint for kubernetes_service run

--- kubernetes_service/provider.tf
+++ kubernetes_service/provider.tf
@@ -1,6 +1,6 @@
 provider "helm" {
   kubernetes {
-    host                   = "https://${local.services.apiserver.ip}:${local.host_ports.apiserver}"
+    host                   = "https://${local.services.service_apiserver.ip}:${local.host_ports.apiserver_backend}"
     client_certificate     = data.terraform_remote_state.client.outputs.kubernetes_admin.cert_pem
     client_key             = data.terraform_remote_state.client.outputs.kubernetes_admin.private_key_pem
     cluster_ca_certificate = data.terraform_remote_state.client.outputs.kubernetes_admin.ca_cert_pem
