860217de25824c21341bffe3ac434bdc294df937
randomcoww <averyfuzzybunny@gmail.com>
2025-01-29 01:13:09 -0800

    revert cloudflare terraform to 4.51.0

--- cluster_resources/versions.tf
+++ cluster_resources/versions.tf
@@ -9,7 +9,7 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "5.0.0"
+      version = "4.51.0"
     }
     tailscale = {
       source  = "tailscale/tailscale"
