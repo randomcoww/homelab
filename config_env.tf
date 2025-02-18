f212ee4822df90c7365ecf5c415bfd55c524fdee
randomcoww <averyfuzzybunny@gmail.com>
2025-02-18 11:23:58 -0800

    add versioning to fcos tags

--- config_env.tf
+++ config_env.tf
@@ -138,7 +138,7 @@ locals {
   }
 
   pxeboot_images = {
-    coreos = "fedora-coreos-41.20250220" # randomcoww/fedora-coreos-config-custom
+    coreos = "fedora-coreos-41.20250210.2043" # randomcoww/fedora-coreos-config-custom
   }
 
   kubernetes = {
