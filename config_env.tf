ab7c862130a49b4dd2fc94601efc137ab37a76cf
randomcoww <averyfuzzybunny@gmail.com>
2025-02-18 10:57:30 -0800

    test new tag format for fcos

--- config_env.tf
+++ config_env.tf
@@ -138,7 +138,7 @@ locals {
   }
 
   pxeboot_images = {
-    coreos = "fedora-coreos-2025021812" # randomcoww/fedora-coreos-config-custom
+    coreos = "fedora-coreos-41.20250218.12" # randomcoww/fedora-coreos-config-custom
   }
 
   kubernetes = {
