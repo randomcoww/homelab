234e3a98f922a794c77cedfd603a3fdc06fea10e
randomcoww <averyfuzzybunny@gmail.com>
2025-03-13 00:46:53 -0700

    re-add runtime config step

---
+++ runtime_config/local_file.tf
@@ -0,0 +1,10 @@
+resource "local_sensitive_file" "config" {
+  for_each = {
+    for _, f in data.terraform_remote_state.ignition.outputs.remote_files[var.host_key] :
+    f.path => f
+  }
+
+  content         = each.value.contents
+  file_permission = format("%04o", lookup(each.value, "mode", 384))
+  filename        = each.key
+}
