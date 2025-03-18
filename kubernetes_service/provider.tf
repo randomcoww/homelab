d50edc4ab32112c2a9cb623c368f2739e57beb51
randomcoww <averyfuzzybunny@gmail.com>
2025-03-17 19:34:21 -0700

    set insecure TLS for minio provider

--- kubernetes_service/provider.tf
+++ kubernetes_service/provider.tf
@@ -11,4 +11,8 @@ provider "minio" {
   minio_server   = "${local.services.minio.ip}:${local.service_ports.minio}"
   minio_user     = data.terraform_remote_state.sr.outputs.minio.access_key_id
   minio_password = data.terraform_remote_state.sr.outputs.minio.secret_access_key
+  minio_ssl      = true
+  # CA needs to be provided as a file which in inconvenient for an argument to a provider
+  minio_insecure = true
+  # minio_cert_file = "outputs/trusted-ca.crt"
 }
