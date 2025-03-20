e45d031fe8875abdffe65c58d805656c4eeca554
randomcoww <averyfuzzybunny@gmail.com>
2025-03-20 01:20:08 -0700

    cleanup

--- bootstrap_client/provider.tf
+++ bootstrap_client/provider.tf
@@ -1,11 +1,10 @@
 locals {
-  matchbox_endpoint     = data.terraform_remote_state.bootstrap-server.outputs.matchbox_endpoint
-  matchbox_api_endpoint = "127.0.0.1:${local.service_ports.matchbox_api}"
-  image_store_endpoint  = "${data.terraform_remote_state.bootstrap-server.outputs.matchbox_endpoint}/assets"
+  matchbox_endpoint    = data.terraform_remote_state.bootstrap-server.outputs.matchbox_endpoint
+  image_store_endpoint = "${data.terraform_remote_state.bootstrap-server.outputs.matchbox_endpoint}/assets"
 }
 
 provider "matchbox" {
-  endpoint    = local.matchbox_api_endpoint
+  endpoint    = "127.0.0.1:${local.service_ports.matchbox_api}"
   client_cert = data.terraform_remote_state.bootstrap-server.outputs.matchbox.client_cert.cert_pem
   client_key  = data.terraform_remote_state.bootstrap-server.outputs.matchbox.client_cert.private_key_pem
   ca          = data.terraform_remote_state.bootstrap-server.outputs.matchbox.ca.cert_pem
--- matchbox_client/provider.tf
+++ matchbox_client/provider.tf
@@ -1,11 +1,10 @@
 locals {
-  matchbox_endpoint     = "https://${local.services.matchbox.ip}:${local.service_ports.matchbox}"
-  matchbox_api_endpoint = "${local.services.matchbox_api.ip}:${local.service_ports.matchbox_api}"
-  image_store_endpoint  = "https://${local.services.minio.ip}:${local.service_ports.minio}/${local.minio.data_buckets.boot.name}"
+  matchbox_endpoint    = "https://${local.services.matchbox.ip}:${local.service_ports.matchbox}"
+  image_store_endpoint = "https://${local.services.minio.ip}:${local.service_ports.minio}/${local.minio.data_buckets.boot.name}"
 }
 
 provider "matchbox" {
-  endpoint    = local.matchbox_api_endpoint
+  endpoint    = "${local.services.matchbox_api.ip}:${local.service_ports.matchbox_api}"
   client_cert = data.terraform_remote_state.client.outputs.matchbox_client.cert_pem
   client_key  = data.terraform_remote_state.client.outputs.matchbox_client.private_key_pem
   ca          = data.terraform_remote_state.client.outputs.matchbox_client.ca_cert_pem
