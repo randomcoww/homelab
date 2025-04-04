ec6c78d1d3b5198a347e61893633ac08dda28b18
randomcoww <averyfuzzybunny@gmail.com>
2025-04-03 18:05:58 -0700

    update qrcode image

--- config_env.tf
+++ config_env.tf
@@ -134,7 +134,7 @@ locals {
     lldap              = "ghcr.io/lldap/lldap:2025-04-01-alpine-rootless"
     keydb              = "docker.io/eqalpha/keydb:alpine_x86_64_v6.3.4"
     clickhouse         = "docker.io/clickhouse/clickhouse-server:25.3.2.39-alpine"
-    qrcode_generator   = "ghcr.io/randomcoww/qrcode-generator:v20250210.0031"
+    qrcode_generator   = "ghcr.io/randomcoww/qrcode-generator:v20250404.0103"
     rclone             = "docker.io/rclone/rclone:1.69.1"
     s3fs               = "ghcr.io/randomcoww/s3fs:v20250401.2322"
     mountpoint         = "ghcr.io/randomcoww/mountpoint-s3:v20250401.2309"
