5ffa0dff47887b37bf02f8a5ad4f6063849381b9
randomcoww <averyfuzzybunny@gmail.com>
2025-04-05 18:49:00 -0700

    configure nginx for audioserve

--- kubernetes_service/template_media.tf
+++ kubernetes_service/template_media.tf
@@ -40,8 +40,8 @@ module "audioserve" {
     "--read-playlist",
   ]
   service_hostname          = local.kubernetes_ingress_endpoints.audioserve
-  ingress_class_name        = local.ingress_classes.ingress_nginx_external
-  nginx_ingress_annotations = local.nginx_ingress_auth_annotations
+  ingress_class_name        = local.ingress_classes.ingress_nginx
+  nginx_ingress_annotations = local.nginx_ingress_annotations
 
   s3_endpoint          = "https://${local.services.cluster_minio.ip}:${local.service_ports.minio}"
   s3_bucket            = minio_s3_bucket.data["music"].id
