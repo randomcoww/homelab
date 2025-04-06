ec3cadbbbbbbae417c1cb2233cc0905fe36e41bd
randomcoww <averyfuzzybunny@gmail.com>
2025-04-05 23:07:36 -0700

    update hostapd options

--- kubernetes_service/template_network.tf
+++ kubernetes_service/template_network.tf
@@ -155,34 +155,31 @@ module "hostapd" {
     }
   }
   # https://w1.fi/cgit/hostap/plain/hostapd/hostapd.conf
+  # https://blog.fraggod.net/2017/04/27/wifi-hostapd-configuration-for-80211ac-networks.html
   config = {
     country_code                 = "US"
     channel                      = 149 # one of 36 44 52 60 100 108 116 124 132 140 149 157 184 192
     vht_oper_centr_freq_seg0_idx = 155 # channel+6 and one of 42 58 106 122 138 155
+    vht_oper_chwidth             = 1
     ssid                         = random_password.hostapd-ssid.result
     sae_password                 = random_password.hostapd-password.result
     interface                    = "wlan0"
     bridge                       = "br-lan"
     driver                       = "nl80211"
     noscan                       = 1
-    preamble                     = 1
     wpa                          = 2
     wpa_key_mgmt                 = "SAE"
     wpa_pairwise                 = "CCMP"
     hw_mode                      = "a"
-    require_ht                   = 1
-    require_vht                  = 1
     ieee80211n                   = 1
     ieee80211ax                  = 1
     ieee80211d                   = 1
-    ieee80211h                   = 0
     ieee80211w                   = 2
-    vht_oper_chwidth             = 1
     ignore_broadcast_ssid        = 0
     auth_algs                    = 1
     wmm_enabled                  = 1
-    disassoc_low_ack             = 0
-    ap_max_inactivity            = 900
+    require_ht                   = 1
+    require_vht                  = 1
     ht_capab = "[${join("][", [
       "HT40-", "HT40+", "SHORT-GI-20", "SHORT-GI-40",
       "LDPC", "TX-STBC", "RX-STBC1", "MAX-AMSDU-7935",
