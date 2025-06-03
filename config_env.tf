eb0d35ed67aa90a62b70f185464884ba026ccfb7
randomcoww <averyfuzzybunny@gmail.com>
2025-06-03 10:41:50 -0700

    use nvidia driver from fcos

--- config_env.tf
+++ config_env.tf
@@ -138,7 +138,7 @@ locals {
     mountpoint              = "ghcr.io/randomcoww/mountpoint-s3:v1.18.0.20250601.2309"
     audioserve              = "docker.io/izderadicka/audioserve:latest"
     sunshine_desktop        = "ghcr.io/randomcoww/sunshine-desktop:v2025.404.171304.20250404.2119"
-    nvidia_driver           = "ghcr.io/randomcoww/nvidia-driver-container:v570.124.06-fedora41"
+    nvidia_driver           = "registry.gitlab.com/container-toolkit-fcos/driver:570.133.20-fedora42"
     stork_agent             = "ghcr.io/randomcoww/stork-agent:v20250501.2122"
     steamcmd                = "ghcr.io/randomcoww/steamcmd:v20250601.2322"
     vaultwarden             = "docker.io/vaultwarden/server:1.34.1-alpine"
