66af617a3266b2e323447a806cca8c49dc8ba089
randomcoww <averyfuzzybunny@gmail.com>
2025-06-18 16:48:34 -0700

    update sunshine image

--- config_env.tf
+++ config_env.tf
@@ -137,7 +137,7 @@ locals {
     rclone                  = "docker.io/rclone/rclone:1.70.0"
     mountpoint              = "ghcr.io/randomcoww/mountpoint-s3:v1.18.0.20250601.2309"
     audioserve              = "docker.io/izderadicka/audioserve:latest"
-    sunshine_desktop        = "ghcr.io/randomcoww/sunshine-desktop:v2025.611.5625.20250612.2147"
+    sunshine_desktop        = "ghcr.io/randomcoww/sunshine-desktop:v2025.617.220707.20250618.2330"
     nvidia_driver           = "ghcr.io/randomcoww/nvidia-driver-container:v570.153.02-fedora42"
     stork_agent             = "ghcr.io/randomcoww/stork-agent:v20250603.1911"
     steamcmd                = "ghcr.io/randomcoww/steamcmd:v20250612.2155"
