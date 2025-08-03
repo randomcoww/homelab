3141be23adbccf8c106f94460de2e2b83cabbcf0
randomcoww <averyfuzzybunny@gmail.com>
2025-08-03 15:13:42 -0700

    remove custom build with nvidia-drm

--- kubernetes_service/modules/nvidia_driver/manifests.tf
+++ kubernetes_service/modules/nvidia_driver/manifests.tf
@@ -9,34 +9,7 @@ module "metadata" {
   release     = var.release
   app_version = split(":", var.images.nvidia_driver)[1]
   manifests = {
-    "templates/daeonset.yaml"  = module.daemonset.manifest
-    "templates/configmap.yaml" = module.configmap.manifest
-    # "templates/poddisruptionbudget.yaml" = yamlencode({
-    #   apiVersion = "policy/v1"
-    #   kind       = "PodDisruptionBudget"
-    #   metadata = {
-    #     name = var.name
-    #   }
-    #   spec = {
-    #     maxUnavailable             = 0
-    #     unhealthyPodEvictionPolicy = "AlwaysAllow"
-    #     selector = {
-    #       matchLabels = {
-    #         app = var.name
-    #       }
-    #     }
-    #   }
-    # })
-  }
-}
-
-module "configmap" {
-  source  = "../../../modules/configmap"
-  name    = var.name
-  app     = var.name
-  release = var.release
-  data = {
-    basename(local.init_path) = file("${path.module}/templates/${basename(local.init_path)}")
+    "templates/daeonset.yaml" = module.daemonset.manifest
   }
 }
 
@@ -45,46 +18,22 @@ module "daemonset" {
   name    = var.name
   app     = var.name
   release = var.release
-  annotations = {
-    "checksum/configmap" = sha256(module.configmap.manifest)
-  }
   template_spec = {
     priorityClassName = "system-node-critical"
     containers = [
       {
         name  = var.name
         image = var.images.nvidia_driver
-        command = [
-          "bash",
-          "-c",
-          <<-EOF
-          set -e
-
-          echo -e "$INIT" > ${local.init_path}
-          chmod +x ${local.init_path}
-          exec ${local.init_path} init --accept-license
-          EOF
-        ]
         securityContext = {
           privileged = true
         }
-        env = concat([
+        env = [
           for _, e in var.extra_envs :
           {
             name  = e.name
             value = tostring(e.value)
           }
-          ], [
-          {
-            name = "INIT"
-            valueFrom = {
-              configMapKeyRef = {
-                name = var.name
-                key  = basename(local.init_path)
-              }
-            }
-          },
-        ])
+        ]
         volumeMounts = [
           {
             name      = "var-log"
