3cfb0c406fc7d8a576d46c888a049b04a1e769f9
randomcoww <averyfuzzybunny@gmail.com>
2025-09-09 15:33:07 -0700

    migrate rolling reboot state to remote

---
+++ rolling_reboot/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket                      = "terraform"
+    key                         = "state/rolling-reboot-0.tfstate"
+    region                      = "auto"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+  }
+}
