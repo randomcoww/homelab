a8707f13bbc6011f2ebeeea1554236541f873286
randomcoww <averyfuzzybunny@gmail.com>
2025-09-14 12:30:25 -0700

    update coreos image

--- config_env.tf
+++ config_env.tf
@@ -141,7 +141,7 @@ locals {
   }
 
   pxeboot_images = {
-    coreos = "fedora-coreos-42.20250913.17" # randomcoww/fedora-coreos-config
+    coreos = "fedora-coreos-42.20250914.19" # randomcoww/fedora-coreos-config
   }
 
   host_ports = {
