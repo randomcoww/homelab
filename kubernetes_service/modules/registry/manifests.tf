4d07cfc45b77912fabbcefa0f3f3243a7fd360bd
randomcoww <averyfuzzybunny@gmail.com>
2025-09-17 18:12:15 -0700

    tweak

--- kubernetes_service/modules/registry/manifests.tf
+++ kubernetes_service/modules/registry/manifests.tf
@@ -54,7 +54,7 @@ module "metadata" {
 
                       update-ca-certificates
                       exec registry garbage-collect \
-                        --delete-untagged=true \
+                        --delete-untagged \
                         ${local.config_path}
                       EOF
                     ]
