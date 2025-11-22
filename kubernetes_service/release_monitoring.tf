dcd12ed9a4aeda06009ffc2c9cb1e346305001b4
randomcoww <randomcoww@users.noreply.github.com>
2025-11-22 08:23:38 -0800

    Update minor-update (#688)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- kubernetes_service/release_monitoring.tf
+++ kubernetes_service/release_monitoring.tf
@@ -42,7 +42,7 @@ resource "helm_release" "prometheus" {
   chart            = "prometheus"
   wait             = false
   wait_for_jobs    = false
-  version          = "27.45.0"
+  version          = "27.46.0"
   max_history      = 2
   timeout          = local.kubernetes.helm_release_timeout
   values = [
