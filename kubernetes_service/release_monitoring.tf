6f5e4868d8866837693b8a3140774e142162bed5
randomcoww <randomcoww@users.noreply.github.com>
2026-01-01 08:28:49 -0800

    Update Helm release prometheus to v28 (#741)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- kubernetes_service/release_monitoring.tf
+++ kubernetes_service/release_monitoring.tf
@@ -42,7 +42,7 @@ resource "helm_release" "prometheus" {
   chart            = "prometheus"
   wait             = false
   wait_for_jobs    = false
-  version          = "27.52.0"
+  version          = "28.0.0"
   max_history      = 2
   timeout          = local.kubernetes.helm_release_timeout
   values = [
