111f04b2e47bddae77189eabb29814a2d7d1080d
randomcoww <randomcoww@users.noreply.github.com>
2026-01-23 08:32:03 -0800

    Update renovate (#767)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- kubernetes_service/release_kube_dns.tf
+++ kubernetes_service/release_kube_dns.tf
@@ -71,7 +71,7 @@ resource "helm_release" "kube-dns" {
   create_namespace = true
   wait             = false
   wait_for_jobs    = false
-  version          = "1.45.1"
+  version          = "1.45.2"
   max_history      = 2
   timeout          = local.kubernetes.helm_release_timeout
   values = [
