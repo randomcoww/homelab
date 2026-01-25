f608096687fdd215bf110f86489b660e533bc57a
randomcoww <averyfuzzybunny@gmail.com>
2026-01-24 17:59:24 -0800

    update etcd-wrapper and coreos image

--- config_env.tf
+++ config_env.tf
@@ -109,7 +109,7 @@ locals {
     kube_controller_manager = "registry.k8s.io/kube-controller-manager:v1.35.0@sha256:3e343fd915d2e214b9a68c045b94017832927edb89aafa471324f8d05a191111"
     kube_scheduler          = "registry.k8s.io/kube-scheduler:v1.35.0@sha256:0ab622491a82532e01876d55e365c08c5bac01bcd5444a8ed58c1127ab47819f"
     etcd                    = "registry.k8s.io/etcd:v3.6.7@sha256:70cd5d29d2efcbc4c15f2a63183fd537aae77ddbc46b3b97a8a97bc8751ec3b4"
-    etcd_wrapper            = "ghcr.io/randomcoww/etcd-wrapper:v0.5.20@sha256:b89224fc8bbb3824a4857af25dd4d6aa068caa1c2c6cd819965285d29a935405"
+    etcd_wrapper            = "ghcr.io/randomcoww/etcd-wrapper:v0.5.21@sha256:e3401048a8dfa3bd12210b5a1b56d1d53513170e20c1ed0a93a67d49500129e6"
     # tier 1
     kube_proxy         = "registry.k8s.io/kube-proxy:v1.35.0@sha256:c818ca1eff765e35348b77e484da915175cdf483f298e1f9885ed706fcbcb34c"
     flannel            = "ghcr.io/flannel-io/flannel:v0.28.0@sha256:adecdcb715b153ef4fadda24142f85556818b6b75170a9dae83ff82995183c86"
@@ -150,7 +150,7 @@ locals {
   host_images = {
     for name, tag in {
       # these fields are updated by renovate - don't use var substitutions
-      default = "fedora-coreos-43.20260123.20" # renovate: randomcoww/fedora-coreos-config-custom
+      default = "fedora-coreos-43.20260124.22" # renovate: randomcoww/fedora-coreos-config-custom
     } :
     name => {
       kernel = "${tag}-live-kernel.$${buildarch:uristring}"
