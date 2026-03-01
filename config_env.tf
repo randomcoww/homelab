ca54d35be77a8b547154fb3158e55b3c8b3dcbba
randomcoww <averyfuzzybunny@gmail.com>
2026-02-28 18:04:51 -0800

    update coreos image

--- config_env.tf
+++ config_env.tf
@@ -150,7 +150,7 @@ locals {
   host_images = {
     for name, tag in {
       # these fields are updated by renovate - don't use var substitutions
-      default = "fedora-coreos-43.20260217.09" # renovate: randomcoww/fedora-coreos-config-custom
+      default = "fedora-coreos-43.20260301.01" # renovate: randomcoww/fedora-coreos-config-custom
     } :
     name => {
       kernel = "${tag}-live-kernel.$${buildarch:uristring}"
