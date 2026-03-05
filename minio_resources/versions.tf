9f847c26e3882bbdc2bfc78d67d9b374b6b7617a
randomcoww <randomcoww@users.noreply.github.com>
2026-03-05 10:06:37 -0800

    Update renovate (#816)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- minio_resources/versions.tf
+++ minio_resources/versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_providers {
     minio = {
       source  = "aminueza/minio"
-      version = "3.25.0"
+      version = "3.26.0"
     }
     tls = {
       source  = "opentofu/tls"
