a042f3a43a3454fbbe65664dff4f4f53abdf68f7
randomcoww <randomcoww@users.noreply.github.com>
2026-03-09 09:57:10 -0700

    Update renovate (#820)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- kubernetes_service/release_system.tf
+++ kubernetes_service/release_system.tf
@@ -131,7 +131,7 @@ resource "helm_release" "traefik" {
   create_namespace = true
   wait             = false
   wait_for_jobs    = false
-  version          = "39.0.4"
+  version          = "39.0.5"
   max_history      = 2
   timeout          = local.kubernetes.helm_release_timeout
   values = [
