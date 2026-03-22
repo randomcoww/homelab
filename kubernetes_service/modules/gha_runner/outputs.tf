b44a430d9559a57656fc1daf39b46a42c1cb68a1
randomcoww <averyfuzzybunny@gmail.com>
2026-03-22 14:31:12 -0700

    fix oci chart depName

--- kubernetes_service/modules/gha_runner/outputs.tf
+++ kubernetes_service/modules/gha_runner/outputs.tf
@@ -69,7 +69,7 @@ output "flux_manifests" {
           interval = "15m"
           url      = "oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller"
           ref = {
-            tag = "0.14.0" # renovate: datasource=docker depName=ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller
+            tag = "0.14.0" # renovate: datasource=helm depName=ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller
           }
         }
       },
@@ -84,7 +84,7 @@ output "flux_manifests" {
           interval = "15m"
           url      = "oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set"
           ref = {
-            tag = "0.14.0" # renovate: datasource=docker depName=ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
+            tag = "0.14.0" # renovate: datasource=helm depName=ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
           }
         }
       },
