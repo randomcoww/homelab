d31681a22ae008c6cdd2a8735ddeedcd9bca63cc
randomcoww <randomcoww@users.noreply.github.com>
2026-05-06 15:23:44 +0900

    Update renovate (#912)

--- helm_release/modules/prometheus/outputs.tf
+++ helm_release/modules/prometheus/outputs.tf
@@ -28,7 +28,7 @@ output "releases" {
           chart = {
             spec = {
               chart   = "prometheus"
-              version = "29.5.0" # renovate: datasource=helm depName=prometheus registryUrl=https://prometheus-community.github.io/helm-charts
+              version = "29.6.0" # renovate: datasource=helm depName=prometheus registryUrl=https://prometheus-community.github.io/helm-charts
               sourceRef = {
                 kind = "HelmRepository"
                 name = var.name
