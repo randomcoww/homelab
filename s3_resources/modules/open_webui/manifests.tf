2b444b9af8736449a3fb7a847324adabf91abfcd
randomcoww <averyfuzzybunny@gmail.com>
2026-05-31 23:23:53 -0700

    pass var.releases where available

--- s3_resources/modules/open_webui/manifests.tf
+++ s3_resources/modules/open_webui/manifests.tf
@@ -272,7 +272,7 @@ module "minio-user-secret" {
   name      = "${var.name}-minio-user-secret"
   namespace = var.namespace
   app       = var.name
-  release   = "0.1.0"
+  release   = var.release
   data = merge({
     AWS_ACCESS_KEY_ID     = var.minio_user.id
     AWS_SECRET_ACCESS_KEY = var.minio_user.secret
