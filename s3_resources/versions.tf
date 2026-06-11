ef651f5f135f0c1804164a40d53fa5b243928c9f
randomcoww <randomcoww@users.noreply.github.com>
2026-06-11 12:38:45 -0700

    Update renovate (#978)

--- s3_resources/versions.tf
+++ s3_resources/versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_providers {
     minio = {
       source  = "aminueza/minio"
-      version = "3.38.0"
+      version = "3.38.1"
     }
     tls = {
       source  = "opentofu/tls"
