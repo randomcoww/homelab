7280ce910efef1a0eddb304c8a8750980ed2794e
randomcoww <randomcoww@users.noreply.github.com>
2026-07-01 09:31:17 -0700

    Update renovate (#1005)

--- s3_resources/versions.tf
+++ s3_resources/versions.tf
@@ -25,7 +25,7 @@ terraform {
     }
     kubernetes = {
       source  = "opentofu/kubernetes"
-      version = "3.2.0"
+      version = "3.2.1"
     }
     helm = {
       source  = "opentofu/helm"
