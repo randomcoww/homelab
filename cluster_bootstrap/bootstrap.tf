e120250c1a3a46bca4586afcdc170945e261ee3d
randomcoww <randomcoww@users.noreply.github.com>
2026-07-16 15:40:13 +0000

    Update renovate

--- cluster_bootstrap/bootstrap.tf
+++ cluster_bootstrap/bootstrap.tf
@@ -222,7 +222,7 @@ resource "helm_release" "kube-dns" {
   create_namespace = true
   wait             = true
   wait_for_jobs    = false
-  version          = "1.46.0"
+  version          = "1.46.1"
   max_history      = 2
   timeout          = local.kubernetes.helm_release_timeout
   values = [
