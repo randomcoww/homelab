b7c267acc7a065cbb6f7b0d5248ff335dfba0ba6
randomcoww <averyfuzzybunny@gmail.com>
2026-07-20 09:49:20 -0700

    update fluxcd bucket permissions - needs only read

--- s3_resources/config_minio.tf
+++ s3_resources/config_minio.tf
@@ -123,10 +123,7 @@ locals {
             Effect = "Allow"
             Action = [
               "s3:GetObject",
-              "s3:PutObject",
               "s3:ListBucket",
-              "s3:DeleteObject",
-              "s3:AbortMultipartUpload",
             ]
             buckets = [
               "fluxcd",
