6fb310941b4a3c65f6951e499d9372bfbd5d83b0
randomcoww <averyfuzzybunny@gmail.com>
2026-07-26 01:23:27 -0700

    move cert-manager to cluster_bootstrap

---
+++ cluster_bootstrap/cert_manager.tf
@@ -0,0 +1,112 @@
+resource "helm_release" "cert-manager" {
+  name             = local.endpoints.cert_manager.name
+  namespace        = local.endpoints.cert_manager.namespace
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  create_namespace = true
+  wait             = true
+  wait_for_jobs    = false
+  version          = "1.21.0"
+  max_history      = 2
+  timeout          = local.kubernetes.helm_release_timeout
+  values = [
+    yamlencode({
+      replicaCount = 2
+      deploymentAnnotations = {
+        "certmanager.k8s.io/disable-validation" = "true"
+      }
+      crds = {
+        enabled = false
+      }
+      enableCertificateOwnerRef = true
+      config = {
+        enableGatewayAPI = true
+      }
+      prometheus = {
+        enabled = true
+        servicemonitor = {
+          enabled = true
+        }
+      }
+      webhook = {
+        replicaCount = 2
+        resources = {
+          requests = {
+            memory = "256Mi"
+          }
+          limits = {
+            memory = "256Mi"
+          }
+        }
+      }
+      resources = {
+        requests = {
+          memory = "256Mi"
+        }
+        limits = {
+          memory = "256Mi"
+        }
+      }
+      cainjector = {
+        enabled      = true
+        replicaCount = 2
+        resources = {
+          requests = {
+            memory = "256Mi"
+          }
+          limits = {
+            memory = "256Mi"
+          }
+        }
+      }
+      startupapicheck = {
+        enabled = true
+      }
+      extraArgs = [
+        "--dns01-recursive-nameservers-only",
+        "--dns01-recursive-nameservers=${join(",", [
+          for _, d in local.upstream_dns :
+          "${d.ip}:53"
+        ])}",
+      ]
+      podDnsConfig = {
+        options = [
+          {
+            name  = "ndots"
+            value = "2"
+          },
+        ]
+      }
+    }),
+  ]
+  depends_on = [
+    kubernetes_labels.labels,
+    helm_release.cert-manager-crds,
+  ]
+}
+
+resource "helm_release" "cert-manager-csi-driver" {
+  name             = "${local.endpoints.cert_manager.name}-csi-driver"
+  namespace        = local.endpoints.cert_manager.namespace
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager-csi-driver"
+  create_namespace = true
+  wait             = true
+  wait_for_jobs    = false
+  version          = "0.15.0"
+  max_history      = 2
+  timeout          = local.kubernetes.helm_release_timeout
+  values = [
+    yamlencode({
+      metrics = {
+        enabled = true
+        podmonitor = {
+          enabled = true
+        }
+      }
+    }),
+  ]
+  depends_on = [
+    kubernetes_labels.labels,
+  ]
+}
