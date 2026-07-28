10ab6c77efcb7c2b59455bf00726df6b1e15911c
randomcoww <randomcoww@users.noreply.github.com>
2026-07-27 17:24:09 -0700

    Update reg.cluster.internal/randomcoww/qrcode-resource Docker tag to v1785197353 (#1038)

--- config_env.tf
+++ config_env.tf
@@ -101,7 +101,7 @@ locals {
     gha_runner       = "ghcr.io/actions/actions-runner:2.336.0@sha256:0cfdcc701ce933c6d243c6b0b2da767366dc9f2e99961d4c3754b0b78084cdda"
     litestream       = "docker.io/litestream/litestream:0.5.15@sha256:f45ca298a567bef6edd23d43429b5f80721473a9a9719e467f11d7888999403e"
     hostapd          = "reg.cluster.internal/randomcoww/hostapd:v2.11.1785197412@sha256:59292fd54e70d5d7276a20afe555fc552774dd0dcbec74c5577e10e87eea2103"
-    qrcode_generator = "reg.cluster.internal/randomcoww/qrcode-resource:v1784562851@sha256:d4140ca16b874f2ebc8288c2244c941ea2404bc2fca2cb44a2afde948fddb8bb"
+    qrcode_generator = "reg.cluster.internal/randomcoww/qrcode-resource:v1785197353@sha256:3b36ada0e19ab5c52db7b63ae7526b3e90ef421505f3a2011bbc7cfd720a592e"
     llama_cpp_vulkan = "reg.cluster.internal/randomcoww/llama-swap-ffmpeg:unified-vulkan-2026-07-27.1785196925@sha256:5ea440407d825d6e0495be30fc2efe5b41d689c9759bb5bf9617ed7dd1a45461"
     searxng          = "ghcr.io/searxng/searxng:latest@sha256:d0aaeb14880e6e92bde1518fcc7261e995783367d63d95203383607bef9c6516"
     lldap            = "ghcr.io/lldap/lldap:v0.6.3-alpine-rootless@sha256:ba2c50930ea998eefd5454aa678a7977448019248b1827da87d330df0b71c284"
