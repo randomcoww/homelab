840541582b37f5bd52f470cb173639b662a4892f
randomcoww <averyfuzzybunny@gmail.com>
2026-07-29 17:05:01 -0700

    define outputs with resource generating it

--- cluster_bootstrap/minio.tf
+++ cluster_bootstrap/minio.tf
@@ -31,3 +31,11 @@ module "minio" {
     helm_release.prometheus-operator-crds,
   ]
 }
+
+output "minio" {
+  value = {
+    access_key_id     = random_password.minio-access-key-id.result
+    secret_access_key = random_password.minio-secret-access-key.result
+  }
+  sensitive = true
+}
--- cluster_bootstrap/outputs.tf
+++
@@ -1,9 +0,0 @@
-# minio
-
-output "minio" {
-  value = {
-    access_key_id     = random_password.minio-access-key-id.result
-    secret_access_key = random_password.minio-secret-access-key.result
-  }
-  sensitive = true
-}
