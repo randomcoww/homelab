f70d6ba744e4cf6a6fd948e3b2fbfd0226c04a5a
randomcoww <randomcoww@users.noreply.github.com>
2026-08-04 16:12:53 +0000

    Update renovate

--- bucket_ops/modules/prometheus/outputs.tf
+++ bucket_ops/modules/prometheus/outputs.tf
@@ -26,7 +26,7 @@ output "manifests" {
           chart = {
             spec = {
               chart   = "kube-prometheus-stack"
-              version = "88.1.3" # renovate: datasource=helm depName=kube-prometheus-stack registryUrl=https://prometheus-community.github.io/helm-charts
+              version = "88.1.4" # renovate: datasource=helm depName=kube-prometheus-stack registryUrl=https://prometheus-community.github.io/helm-charts
               sourceRef = {
                 kind = "HelmRepository"
                 name = var.name
--- bucket_ops/searxng.tf
+++ bucket_ops/searxng.tf
@@ -6,7 +6,7 @@ module "searxng" {
   images = {
     searxng = {
       repository = "ghcr.io/searxng/searxng"
-      tag        = "latest@sha256:912f24817104f45556da9f271e073aab9cd19d84647b299a85c232cf1f086e6d" # renovate: datasource=docker depName=ghcr.io/searxng/searxng
+      tag        = "latest@sha256:af6bde1e3590f91be5b7c4e393b3bff82ada4b93d60fc10806fe1ed4ba147d4d" # renovate: datasource=docker depName=ghcr.io/searxng/searxng
     }
   }
   searxng_settings = {
