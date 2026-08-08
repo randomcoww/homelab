7fd4fcc446073357774f5923f2a5f0ded8b1e87a
randomcoww <randomcoww@users.noreply.github.com>
2026-08-08 07:34:46 -0700

    Update renovate (#1060)

--- bucket_ops/victoria-metrics.tf
+++ bucket_ops/victoria-metrics.tf
@@ -32,7 +32,7 @@ resource "minio_s3_object" "fluxcd-victoria-metrics" {
             chart = {
               spec = {
                 chart   = "victoria-metrics-k8s-stack"
-                version = "0.90.0" # renovate: datasource=helm depName=victoria-metrics-k8s-stack registryUrl=https://victoriametrics.github.io/helm-charts
+                version = "0.90.1" # renovate: datasource=helm depName=victoria-metrics-k8s-stack registryUrl=https://victoriametrics.github.io/helm-charts
                 sourceRef = {
                   kind = "HelmRepository"
                   name = local.victoria-metrics_name
