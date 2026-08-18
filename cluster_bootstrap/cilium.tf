c3632579b738f57f482e01ebeb7f71200e750cd4
randomcoww <randomcoww@users.noreply.github.com>
2026-08-18 14:34:31 +0000

    Update renovate

--- cluster_bootstrap/cilium.tf
+++ cluster_bootstrap/cilium.tf
@@ -55,7 +55,7 @@ resource "helm_release" "cilium" {
   create_namespace = true
   wait             = true
   wait_for_jobs    = false
-  version          = "1.20.0"
+  version          = "1.20.1"
   max_history      = 2
   timeout          = local.kubernetes.helm_release_timeout
   values = [
