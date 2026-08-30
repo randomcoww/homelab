0e80536c1a8863a0ecf4f8db56953fb351ace59e
randomcoww <averyfuzzybunny@gmail.com>
2026-08-30 16:54:48 -0700

    remove old registry cert - replaced with zot

--- local_credentials/outputs.tf
+++ local_credentials/outputs.tf
@@ -2,14 +2,6 @@ output "ssh_user_cert_authorized_key" {
   value = ssh_user_cert.ssh-client.cert_authorized_key
 }
 
-output "registry_client" {
-  value = {
-    private_key_pem = tls_private_key.registry-client.private_key_pem
-    cert_pem        = tls_locally_signed_cert.registry-client.cert_pem
-  }
-  sensitive = true
-}
-
 output "zot_client" {
   value = {
     private_key_pem = tls_private_key.zot-client.private_key_pem
