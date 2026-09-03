5dcddc2305bb5749fd12b2556a989a2031abdc53
randomcoww <randomcoww@users.noreply.github.com>
2026-09-03 17:35:34 +0000

    Update renovate

--- bucket_ops/sunshine-desktop.tf
+++ bucket_ops/sunshine-desktop.tf
@@ -9,7 +9,7 @@ module "sunshine-desktop" {
     }
     nginx = {
       repository = "docker.io/nginxinc/nginx-unprivileged"
-      tag        = "1.31.4-alpine@sha256:d9083fe47768377ef55dedafd67d4da7c2f2bc2bece7554954f29359deb0dce9" # renovate: datasource=docker depName=docker.io/nginxinc/nginx-unprivileged
+      tag        = "1.31.5-alpine@sha256:aa8c9087d36d93e9d650c5365f883b421e8214aedbad24ade52b844c583358f1" # renovate: datasource=docker depName=docker.io/nginxinc/nginx-unprivileged
     }
   }
   user               = "sunshine"
