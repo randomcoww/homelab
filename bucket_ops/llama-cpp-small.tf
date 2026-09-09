94138913825ff3c0bea16d51fbcd043eaf1af447
randomcoww <averyfuzzybunny@gmail.com>
2026-09-09 15:00:07 -0700

    scale down llama-cpp-small to save memory

--- bucket_ops/llama-cpp-small.tf
+++ bucket_ops/llama-cpp-small.tf
@@ -1,7 +1,7 @@
 module "llama-cpp-small" {
   source    = "./modules/llama-cpp"
   name      = "llama-cpp-small"
-  replicas  = 1
+  replicas  = 0
   namespace = local.llama-cpp_namespace # must be in same namespace as sunshine to share GPU
   images = {
     llama-swap = {
