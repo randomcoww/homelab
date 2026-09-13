21a01be6e2ca464d33bc1c12a83f0d560cb5d3b5
randomcoww <randomcoww@users.noreply.github.com>
2026-09-13 17:19:15 +0000

    Update renovate

--- bucket_ops/searxng.tf
+++ bucket_ops/searxng.tf
@@ -12,7 +12,7 @@ module "searxng" {
   images = {
     searxng = {
       repository = "ghcr.io/searxng/searxng"
-      tag        = "latest@sha256:1f3720f59a0361592dc942e4be50e0ebb63c112d915883fcb78a011995fffb47" # renovate: datasource=docker depName=ghcr.io/searxng/searxng
+      tag        = "latest@sha256:d0a4ca04e68c6d57fe45509ad5a6b10c890350724762ade3abea5363571aa29a" # renovate: datasource=docker depName=ghcr.io/searxng/searxng
     }
   }
   extra_envs = {
