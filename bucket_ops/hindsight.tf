8e742e0506c0a9b2fd479922e5a464d30aa9b5c0
randomcoww <averyfuzzybunny@gmail.com>
2026-09-21 01:06:28 -0700

    use low reasoning for hindsight reflect

--- bucket_ops/hindsight.tf
+++ bucket_ops/hindsight.tf
@@ -12,7 +12,7 @@ module "hindsight" {
   extra_envs = {
     HINDSIGHT_API_LLM_MODEL              = "qwen-3-8-flash-next-low"
     HINDSIGHT_API_LLM_TIMEOUT            = "1800"
-    HINDSIGHT_API_REFLECT_LLM_MODEL      = "qwen-3-8-flash-next"
+    HINDSIGHT_API_REFLECT_LLM_MODEL      = "qwen-3-8-flash-next-low"
     HINDSIGHT_API_REFLECT_WALL_TIMEOUT   = "1800" # default 300
     HINDSIGHT_API_REFLECT_MAX_ITERATIONS = "4"    # default 10
   }
