3b0d284c2bc4cbe28a9e6a7839cc85cd7969d9b7
randomcoww <averyfuzzybunny@gmail.com>
2026-09-21 22:36:55 -0700

    default to 0 sunshine replicas

--- bucket_ops/sunshine-desktop.tf
+++ bucket_ops/sunshine-desktop.tf
@@ -2,7 +2,7 @@ module "sunshine-desktop" {
   source    = "./modules/sunshine-desktop"
   name      = "sunshine-desktop"
   namespace = "default" # must be in same namespace as llama.cpp to share GPU
-  replicas  = 1
+  replicas  = 0
   images = {
     sunshine-desktop = {
       repository = "zot.cluster.internal/randomcoww/sunshine-desktop"
